f1c329401bf7d70761015ed9cce9eb1000ab6ab2
Rafael Vesoloski <134641899+devrafaelvsk@users.noreply.github.com>
2024-08-27 10:44:56 -0300

    Atualizacoes

---
+++ primeiraPagina/index.jsx
@@ -0,0 +1,13 @@
+
+function PrimatyButon() {
+    return <button>Botão</button>
+}
+
+function Conteiner() {
+    return (
+        <div className="salve">
+            <h1></h1>
+            <p>só testando...</p>
+        </div>
+    )
+}
