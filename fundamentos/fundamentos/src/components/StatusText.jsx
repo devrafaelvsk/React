382474b24cd0b6c58d8695707dcc61884502e2bf
Rafael Vesoloski <134641899+devrafaelvsk@users.noreply.github.com>
2024-08-27 21:50:00 -0300

    Atualizacoes

---
+++ fundamentos/fundamentos/src/components/StatusText.jsx
@@ -0,0 +1,3 @@
+export default () => {
+    return <h2>salve parça, 1 + 1 é {1 + 1} tlgd</h2>
+}
---
+++ fundamentos/fundamentos/src/components/Title/index.jsx
@@ -0,0 +1,4 @@
+export default function Title() {
+    const tech = "React"
+    return <h1>{tech} é mt bom!</h1>
+}
